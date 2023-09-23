55ceba6475e6ed360e4f2b0ff98e36c4d45441d4
ProkopevTuskul272 <142722309+ProkopevTuskul272@users.noreply.github.com>
2023-09-23 23:48:38 +0900

    This bot

---
+++ m1l4/main.py
@@ -0,0 +1,79 @@
+import discord
+import random
+import os
+from discord.ext import commands
+import requests    
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    await ctx.send(left + right)
+
+@bot.command()
+async def choise(ctx, *choices: str):
+    await ctx.send(random.choice(choices))
+
+@bot.command()
+async def repeat(ctx, times: int, content="repeating"):
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+@bot.group()
+async def cool(ctx):
+    if ctx.invoked_subcommand is None:
+        await ctx.send(f'No, {ctx.subcommand_passed} is not cool')
+
+@cool.command(name='bot')
+async def _bot(ctx):
+    await ctx.send('Yes, the bot is cool.')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')   
+
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture) 
+
+def get_duck_image_url():    
+    url = 'https://random.dog/woof.json'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def data_fox():
+    image = 'https://randomfox.ca/floof/'
+    res = requests.get(image)
+    data = res.json()
+    return data['image']
+
+@bot.command('fox')
+async def fox(ctx):
+    image_fox = data_fox()
+    await ctx.send(image_fox)
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("heh" * count_heh)
+
+bot.run("MTE0NzgyOTQ3NDAxMjk1ODc1MA.G-AKAL.A88sr67tkV6owagYfjGX2HJ_nWTZb97ShRzJvM")
