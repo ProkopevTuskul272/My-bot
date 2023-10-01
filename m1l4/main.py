c9a0d6a61de69cf4d0584636f456eeac0cebbdd4
mr. lilepicfacetp <142722309+ProkopevTuskul272@users.noreply.github.com>
2023-10-01 14:23:20 +0900

    main.py

--- m1l4/main.py
+++ m1l4/main.py
@@ -9,9 +9,16 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
 
+data = ['ЭКОНОМЬТЕ РЕСУРСЫ', 'РАЗДЕЛЯЙТЕ МУСОР', 'СДАВАЙТЕ ВТОРСЫРЬЁ', 'ВЫБИРАЙТЕ ЭКОЛОГИЧНЫЙ ТРАНСПОРТ', 'ИСПОЛЬЗУЙТЕ ПОВТОРНО И НЕ БЕРИТЕ ЛИШНЕЕ', 'ВНЕДРЯЙТЕ ЭКО-ПРИВЫЧКИ НА РАБОТЕ', 'ОБРАТИТЕ ВНИМАНИЕ НА ПИТАНИЕ', 'ПОСТАРАЙТЕСЬ ОТВЫКНУТЬ ОТ ПЛАСТИКА']
+
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def eco(ctx):
+    eco_message = random.choice(data)
+    await ctx.send(eco_message)
 
 @bot.command()
 async def add(ctx, left: int, right: int):
